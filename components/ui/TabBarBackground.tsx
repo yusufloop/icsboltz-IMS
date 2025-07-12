4caf2f35cea5c566553cc28431c1e484ca29174b
MusToffi <muskingpower95@gmail.com>
2025-07-12 20:34:43 +0800

    Fix TabBarBackground component to return a View instead of undefined

--- components/ui/TabBarBackground.tsx
+++ components/ui/TabBarBackground.tsx
@@ -1,5 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
 // This is a shim for web and Android where the tab bar is generally opaque.
-export default undefined;
+export default function TabBarBackground() {
+  return <View />;
+}
 
 export function useBottomTabOverflow() {
   return 0;
